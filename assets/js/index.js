b6fe7f751945aeabeb8718896d458f59e7175393
László Szücs <laszlo.szucs.948@gmail.com>
2020-07-08 01:50:15 +0200

    transation 0 to auto with Vue (IE11)

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import App from './components/app/index.vue';
-
-import './globals.css';
-
-new Vue({
-  el: '#app',
-  render: h => h(App)
-});
---
+++ src/components/TransitionExpand.vue
@@ -0,0 +1,98 @@
+<script>
+export default {
+  name: `TransitionExpand`,
+  functional: true,
+  props: {
+    afterEnter: {
+      type: Function
+    },
+    afterLeave: {
+      type: Function
+    }
+  },
+  render(createElement, context) {
+    const data = {
+      props: {
+        name: `expand`,
+      },
+      on: {
+        afterEnter(element) {
+          // eslint-disable-next-line no-param-reassign
+          element.style.height = `auto`;
+          if(context.props.afterEnter) {
+            context.props.afterEnter();
+          }
+        },
+        afterLeave(element) {
+          // eslint-disable-next-line no-param-reassign
+          element.style.height = `auto`;
+          if(context.props.afterLeave) {
+            context.props.afterLeave();
+          }
+        },
+        enter(element) {
+          console.log('enter');
+          const { width } = getComputedStyle(element);
+          /* eslint-disable no-param-reassign */
+          element.style.width = width;
+          element.style.position = `absolute`;
+          element.style.visibility = `hidden`;
+          element.style.height = `auto`;
+          /* eslint-enable */
+          const { height } = getComputedStyle(element);
+          /* eslint-disable no-param-reassign */
+          element.style.width = "inherit";
+          element.style.position = "inherit";
+          element.style.visibility = "inherit";
+          element.style.height = 0;
+          /* eslint-enable */
+          // Force repaint to make sure the
+          // animation is triggered correctly.
+          // eslint-disable-next-line no-unused-expressions
+          getComputedStyle(element).height;
+          requestAnimationFrame(() => {
+            // eslint-disable-next-line no-param-reassign
+            element.style.height = height;
+          });
+        },
+        leave(element) {
+          console.log('leave');
+          const { height } = getComputedStyle(element);
+          // eslint-disable-next-line no-param-reassign
+          element.style.height = height;
+          // Force repaint to make sure the
+          // animation is triggered correctly.
+          // eslint-disable-next-line no-unused-expressions
+          getComputedStyle(element).height;
+          requestAnimationFrame(() => {
+            // eslint-disable-next-line no-param-reassign
+            element.style.height = 0;
+          });
+        },
+      },
+    };
+    return createElement(`transition`, data, context.children);
+  },
+};
+</script>
+
+<style scoped>
+* {
+  will-change: height;
+  transform: translateZ(0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
+</style>
+
+<style>
+.expand-enter-active,
+.expand-leave-active {
+  transition: height 1s;
+  overflow: hidden;
+}
+.expand-enter,
+.expand-leave-to {
+  height: 0;
+}
+</style>
